47ee994c378221a9c0c82bcee843cdb0ab3bbd55
racoltdev <racoltdev@gmail.com>
2024-03-07 15:02:00 -0500

    Tests and fixed code

--- Code-1/__tests__/test.app.js
+++ Code-1/__tests__/test.app.js
@@ -1,73 +1,74 @@
 const calculatePrice = require('../calculatePrice'); // Assuming your function is in a separate file
 
 describe('calculatePrice function', () => {
-  test('Valid price, isStudent=false, hasCoupon=false', () => {
-    const price = 100;
-    const isStudent = false;
-    const hasCoupon = false;
-    expect(calculatePrice(price, isStudent, hasCoupon)).toBe(100);
-  });
+	test('Valid price, isStudent=false, hasCoupon=false', () => {
+		const price = 100;
+		const isStudent = false;
+		const hasCoupon = false;
+		expect(calculatePrice(price, isStudent, hasCoupon)).toBe(100);
+	});
 
-  test('Valid price, isStudent=true, hasCoupon=false', () => {
-    const price = 100;
-    const isStudent = true;
-    const hasCoupon = false;
-    expect(calculatePrice(price, isStudent, hasCoupon)).toBe(90);
-  });
+	test('Valid price, isStudent=true, hasCoupon=false', () => {
+		const price = 100;
+		const isStudent = true;
+		const hasCoupon = false;
+		expect(calculatePrice(price, isStudent, hasCoupon)).toBe(90);
+	});
 
-  test('Valid price, isStudent=false, hasCoupon=true', () => {
-    const price = 100;
-    const isStudent = false;
-    const hasCoupon = true;
-    expect(calculatePrice(price, isStudent, hasCoupon)).toBe(95);
-  });
+	test('Valid price, isStudent=false, hasCoupon=true', () => {
+		const price = 100;
+		const isStudent = false;
+		const hasCoupon = true;
+		expect(calculatePrice(price, isStudent, hasCoupon)).toBe(95);
+	});
 
-  test('Valid price, isStudent=true, hasCoupon=true', () => {
-    const price = 100;
-    const isStudent = true;
-    const hasCoupon = true;
-    expect(calculatePrice(price, isStudent, hasCoupon)).toBe(80);
-  });
+	test('Valid price, isStudent=true, hasCoupon=true', () => {
+		const price = 100;
+		const isStudent = true;
+		const hasCoupon = true;
+		expect(calculatePrice(price, isStudent, hasCoupon)).toBe(80);
+	});
 
-  test('Price is a negative number', () => {
-    const price = -10;
-    const isStudent = false;
-    const hasCoupon = false;
-    expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
-  });
+	test('Price is a negative number', () => {
+		const price = -10;
+		const isStudent = false;
+		const hasCoupon = false;
+		expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
+	});
 
-  test('Price is a string', () => {
-    const price = "10";
-    const isStudent = false;
-    const hasCoupon = false;
-    expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
-  });
+	test('Price is a string', () => {
+		const price = "10";
+		const isStudent = false;
+		const hasCoupon = false;
+		expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
+	});
 
-  test('isStudent is not boolean', () => {
-    const price = 100;
-    const isStudent = "true";
-    const hasCoupon = false;
-    expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid isStudent type: It should be either true or false");
-  });
+	test('isStudent is not boolean', () => {
+		const price = 100;
+		const isStudent = "true";
+		const hasCoupon = false;
+		expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid isStudent type: It should be either true or false");
+	});
 
-  test('hasCoupon is not boolean', () => {
-    const price = 100;
-    const isStudent = false;
-    const hasCoupon = "false";
-    expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid hasCoupon type: It should be either true or false");
-  });
+	test('hasCoupon is not boolean', () => {
+		const price = 100;
+		const isStudent = false;
+		const hasCoupon = "false";
+		expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid hasCoupon type: It should be either true or false");
+	});
 
-  test('Output price is less than 80% of input price', () => {
-    const price = 10;
-    const isStudent = false;
-    const hasCoupon = false;
-    expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
-  });
+// Neither of these tests can ever get the error they are looking for
+//	test('Output price is less than 80% of input price', () => {
+//		const price = 10;
+//		const isStudent = false;
+//		const hasCoupon = false;
+//		expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
+//	});
 
-  test('Output price is greater than input price', () => {
-    const price = 600;
-    const isStudent = false;
-    const hasCoupon = false;
-    expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
-  });
+//	test('Output price is greater than input price', () => {
+//		const price = 600;
+//		const isStudent = false;
+//		const hasCoupon = false;
+//		expect(() => calculatePrice(price, isStudent, hasCoupon)).toThrowError("Invalid price: Price must be a positive number.");
+//	});
 });
--- Code-2/bloodPressure.js
+++ Code-2/bloodPressure.js
@@ -1,6 +1,6 @@
 // Important: This code had intentional errors which are detected by the tests cases.
 
-function BloodPressure(patientName, patientAge,systolic,diastolic) {
+function BloodPressure(patientName, patientAge, systolic, diastolic) {
 
   const severityLevels = {
     "NORMAL": 0,
@@ -18,7 +18,7 @@ function BloodPressure(patientName, patientAge,systolic,diastolic) {
         throw new Error("Name must be less than or equal to 50 characters.");
       }
       // Validate age
-      if (patientAge < 0 && patientAge >= 130) {
+      if (patientAge < 0 || patientAge >= 130) {
         throw new Error("Age must be greater than or equal to 0 and less than 130.");
       }
       // validate systolic and diastolic
@@ -29,13 +29,13 @@ function BloodPressure(patientName, patientAge,systolic,diastolic) {
       let systolicDiagnosis, diastolicDiagnosis;
 
       // Assign diagnoses based on readings
-      if (systolic > 120) {
+      if (systolic < 120) {
         systolicDiagnosis = "NORMAL";
       } else if (systolic < 140) {
         systolicDiagnosis = "PREHYPERTENSION";
       } else if (systolic < 160) {
         systolicDiagnosis = "STAGE_1_HBP";
-      } else if (systolic >= 160) {
+      } else if (systolic <= 180) {
         systolicDiagnosis = "STAGE_2_HBP";
       } else if (systolic > 180) {
         systolicDiagnosis = "HYPERTENSIVE_CRISIS";
@@ -46,7 +46,7 @@ function BloodPressure(patientName, patientAge,systolic,diastolic) {
         diastolicDiagnosis = "PREHYPERTENSION";
       } else if (diastolic < 100) {
         diastolicDiagnosis = "STAGE_1_HBP";
-      } else if (diastolic >= 100) {
+      } else if (diastolic <= 110) {
         diastolicDiagnosis = "STAGE_2_HBP";
       } else if (diastolic > 110) {
         diastolicDiagnosis = "HYPERTENSIVE_CRISIS";
